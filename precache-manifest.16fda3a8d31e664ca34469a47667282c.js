54be4a531ed4eaa4cf9a6cce4c089b86ba8672b1
Asmita gaikwad <asmitagaikwad@gmailcom>
2020-09-19 15:31:16 +0530

    Updates

---
+++ precache-manifest.16fda3a8d31e664ca34469a47667282c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e02890c1c401772edd56d1921304599",
+    "url": "/asm.git/index.html"
+  },
+  {
+    "revision": "1ec20182c9fb4f9392f2",
+    "url": "/asm.git/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "ab6c36075be435d4054b",
+    "url": "/asm.git/static/css/main.09fa8d7b.chunk.css"
+  },
+  {
+    "revision": "1ec20182c9fb4f9392f2",
+    "url": "/asm.git/static/js/2.709acfb9.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/asm.git/static/js/2.709acfb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab6c36075be435d4054b",
+    "url": "/asm.git/static/js/main.dee2819a.chunk.js"
+  },
+  {
+    "revision": "72a1f7d2b112ded760c4",
+    "url": "/asm.git/static/js/runtime-main.9e52e596.js"
+  }
+]);
